48e9769ba9182d9ac6929050037fca8ed03d1e44
hawardShin <joushin@student.42seoul.kr>
2024-03-03 12:26:25 +0900

    Feat: 연과관관계 편의 메서드 AND use CasCade

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -31,10 +32,17 @@ public class Order {
 	@JoinColumn(name="member_id") // set fk name member_id
 	private Member member;
 
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
+	//if there is no cascade
+	//persist(orderItemA)
+	//persist(orderItemB)
+	//persist(orderItemC)
+	//persist(order)
 
-	@OneToOne(fetch=FetchType.LAZY)
+	//there is cascade spread persist
+	//only persist(order)
+	@OneToOne(fetch=FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name="delivery_id")
 	private Delivery delivery;
 
@@ -43,4 +51,33 @@ public class Order {
 
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status; //ORDER status, [ORDER, CANCEL]
+	
+	//연관관계 편의 메서드
+	//Association convenience method
+	public void setMember(Member member){
+		this.member = member;
+		member.getOrders().add(this);
+	}
+
+	public void addOrderItem(OrderItem orderItem){
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+
+	}
+
+	public void setDelivery(Delivery delivery){
+		this.delivery = delivery;
+		delivery.setOrder(this);
+	}
+
+	// if there is no set Method you have to code like below
+	// public static void main(String[] args){
+	// 	Member member = new Member();
+	// 	Order order = new Order();
+	//
+	// 	member.getOrders().add(order);
+	// 	order.setMember(member);
+	//  //if method set only
+	//  order.setMember(member);
+	// }
 }
