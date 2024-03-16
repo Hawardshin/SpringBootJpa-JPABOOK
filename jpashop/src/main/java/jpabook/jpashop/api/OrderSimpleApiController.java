143532b10d83093e245989d43a823822392f031f
hawardShin <joushin@student.42seoul.kr>
2024-03-16 16:27:35 +0900

    Feat: solve problem using Dto but still have n+1 problem

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -23,7 +28,7 @@ import java.util.List;
 public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
 
-    @GetMapping("/api/v1/simple- orders")
+    @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
@@ -33,5 +38,42 @@ public class OrderSimpleApiController {
         }
         return all;
         //이 코드를 그대로 사용하면 양방향 연관관계 문제가 발생한다.
+    }
+
+
+    //api 스펙에 맞게 개발 했음.
+    //단, 둘 다 LAZY Loading 으로 인해 쿼리가 너무 많이 호출되는 문제가 있다.
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2(){
+        //ORDER 2개가 조회됨
+        //N+1  -> 1+ N -> 1+ 회원 N  배송 N
+        //N이 2개
+        // 첫번째 쿼리의 결과로 n번 만큼 쿼리가 실행되는 것은 n+1문제라고 부릅니다.
+        //그렇다고 해결하기 위해서 EAGER를 사용해서는 절대 안된다. -> 필요하면 fetch join을 하시길.
+        //지연로딩은 이미 조회된 경우 쿼리 생략함.
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        // 이후 생성자에서
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();//LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();//LAZY 초기화
+
+        }
     }
 }
