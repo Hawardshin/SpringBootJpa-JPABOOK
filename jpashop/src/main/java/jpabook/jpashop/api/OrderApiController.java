b91361094462d83754e00dfeb85ca5254c5f6188
hawardShin <joushin@student.42seoul.kr>
2024-03-17 13:54:20 +0900

    Feat: V2 For one to Many Rapped by Dto but Too many query

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +34,50 @@ public class OrderApiController {
 //            }
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> orderV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+
+    // 주석 친 부분: 이것이 얼핏 보기엔 Dto로 잘 반환한 것 같지만, 그게 아니다. Dto내부에서 엔티티를 반환하는 코드가 있기 때문에 이것도 좋지 못한 방법이다.
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        //        private List<OrderItem> orderItems;
+        private List<OrderItemDto> orderItems;
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            address  = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+//            order.getOrderItems().stream().forEach(o -> o.getItem().getName());//프록시 해결 (OrderItem을 다 Dto로 바꿔야합니다.)
+//            orderItems = order.getOrderItems();
+        }
+    }
+    @Getter
+    static class OrderItemDto{
+        private String itemName;//상품명
+        private int orderPrice; //주문 가격
+        private int count; // 주문 수량
+        public OrderItemDto(OrderItem orderItem){
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getItem().getPrice();
+            count = orderItem.getCount();
+        }
     }
 }
