17af1a8b113eab1791cbb0e5d29eab32f678ec22
hawardShin <joushin@student.42seoul.kr>
2024-03-20 14:21:37 +0900

    Feat: OSIV Error Solution separate QueryService

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,8 @@ import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
+import jpabook.jpashop.service.query.OrderDto;
+import jpabook.jpashop.service.query.OrderQueryService;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -47,13 +49,10 @@ public class OrderApiController {
         return collect;
     }
 
+    private final OrderQueryService orderQueryService;
     @GetMapping("/api/v3/orders")
     public List<OrderDto> orderV3(){
-        List<Order> orders = orderRepository.findAllWithItem();
-        List<OrderDto> collect = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
-        return collect;
+        return orderQueryService.OrderV3();
     }
 
     @GetMapping("/api/v3.1/orders")
@@ -92,36 +91,5 @@ public class OrderApiController {
 
     // 주석 친 부분: 이것이 얼핏 보기엔 Dto로 잘 반환한 것 같지만, 그게 아니다. Dto내부에서 엔티티를 반환하는 코드가 있기 때문에 이것도 좋지 못한 방법이다.
 
-    @Data
-    static class OrderDto {
-        private Long orderId;
-        private String name;
-        private LocalDateTime orderDate;
-        private OrderStatus orderStatus;
-        private Address address;
-        //        private List<OrderItem> orderItems;
-        private List<OrderItemDto> orderItems;
-        public OrderDto(Order order) {
-            orderId = order.getId();
-            name = order.getMember().getName();
-            orderDate = order.getOrderDate();
-            address  = order.getDelivery().getAddress();
-            orderItems = order.getOrderItems().stream()
-                    .map(orderItem -> new OrderItemDto(orderItem))
-                    .collect(Collectors.toList());
-//            order.getOrderItems().stream().forEach(o -> o.getItem().getName());//프록시 해결 (OrderItem을 다 Dto로 바꿔야합니다.)
-//            orderItems = order.getOrderItems();
-        }
-    }
-    @Getter
-    static class OrderItemDto{
-        private String itemName;//상품명
-        private int orderPrice; //주문 가격
-        private int count; // 주문 수량
-        public OrderItemDto(OrderItem orderItem){
-            itemName = orderItem.getItem().getName();
-            orderPrice = orderItem.getItem().getPrice();
-            count = orderItem.getCount();
-        }
-    }
+
 }
